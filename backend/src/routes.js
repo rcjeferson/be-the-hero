c9f9f0816d9de3fa404e8d5daf3ada0b46884ea3
Jeferson Costa <jeferson.costa@instruct.com.br>
2020-03-24 23:54:33 -0300

    feat(sessão): Adiciona sessão
    Adiciona sessão para autenticação da Ong.

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const SessionController = require('./controllers/SessionController')
 const routes = express.Router();
 
+routes.post('/sessions', SessionController.create)
 module.exports = routes;
