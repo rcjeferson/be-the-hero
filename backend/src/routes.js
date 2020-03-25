eaa083b639d4e1995ba3fd1baf30c07658a6dd09
Jeferson Costa <jeferson.costa@instruct.com.br>
2020-03-24 23:55:43 -0300

    feat(ong): Adiciona Ong
    Adiciona gerenciamento de ongs com recursos para criar e listar.

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,6 +1,9 @@
 const express = require('express');
+const OngController = require('./controllers/OngController')
 const SessionController = require('./controllers/SessionController')
 const routes = express.Router();
 
 routes.post('/sessions', SessionController.create)
+routes.get('/ongs', OngController.index);
+routes.post('/ongs', OngController.create);
 module.exports = routes;
