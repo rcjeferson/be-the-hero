919e826d4e9ace7856a314f4f3b0f486b2687006
Jeferson Costa <jeferson.costa@instruct.com.br>
2020-03-26 19:14:55 -0300

    fix(Logon): Pontuação e espaçamento na tela
    Ajusta pontuações e espaçamento na tela de login.

--- frontend/src/pages/Logon/index.js
+++ frontend/src/pages/Logon/index.js
@@ -1,10 +1,11 @@
 import { FiLogIn } from "react-icons/fi";
+import { Link } from 'react-router-dom';
 import React from 'react';
 
 import './styles.css';
 
-import heroesImg from '../../assets/heroes.png'
-import logoImg from '../../assets/logo.svg'
+import heroesImg from '../../assets/heroes.png';
+import logoImg from '../../assets/logo.svg';
 
 export default function Logon() {
   return (
@@ -18,10 +19,10 @@ export default function Logon() {
           <input placeholder="Sua ID" />
           <button type="submit"className="button">Entrar</button>
 
-          <a href="/register">
+          <Link className="back-link" to="/register">
             <FiLogIn size={16} color="#E02041" />
             Não tenho cadastro
-          </a>
+          </Link>
         </form>
       </section>
       <img src={ heroesImg } alt="Heroes" />
