014833328b3b5f5c0ab7e8b9fbb255e109a1b562
Jeferson Costa <jeferson.costa@instruct.com.br>
2020-03-26 22:25:36 -0300

    feat(Logon): Adiciona integração
    Adiciona integração da tela de logon ao Backend para que o usuáro possa
    ver validado e conseguir utilizar a ferramenta.

--- frontend/src/pages/Logon/index.js
+++ frontend/src/pages/Logon/index.js
@@ -1,22 +1,47 @@
 import { FiLogIn } from "react-icons/fi";
-import { Link } from 'react-router-dom';
-import React from 'react';
+import { Link, useHistory } from 'react-router-dom';
+import React, { useState } from 'react';
 
+import api from '../../services/api';
 import './styles.css';
 
 import heroesImg from '../../assets/heroes.png';
 import logoImg from '../../assets/logo.svg';
 
 export default function Logon() {
+  const [id, setId] = useState('');
+
+  const history = useHistory();
+
+  async function handleLogin(e) {
+    e.preventDefault();
+
+    try {
+      const resp = await api.post('sessions', { id });
+
+      localStorage.setItem('ongId', id);
+      localStorage.setItem('ongName', resp.data.name)
+
+      history.push('/profile');
+    } catch (error) {
+      alert('Falha no login! Tente novamente.')
+    }
+  }
+
   return (
     <div className="logon-container">
       <section className="form">
         <img src={ logoImg } alt="Be The Hero" />
 
-        <form>
+        <form onSubmit={ handleLogin }>
           <h1>Faça seu logon</h1>
 
-          <input placeholder="Sua ID" />
+          <input
+            placeholder="Sua ID"
+            value={ id }
+            onChange={ e => setId(e.target.value) }
+          />
+          
           <button type="submit"className="button">Entrar</button>
 
           <Link className="back-link" to="/register">
