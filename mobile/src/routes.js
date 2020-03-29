383f5377be6e2e5d865059d5ee982b04234d89c2
Jeferson Costa <jeferson.costa@instruct.com.br>
2020-03-28 22:05:03 -0300

    feat(mobile): Adiciona estrutura de rotas
    Adiciona estrutura de rotas default do projeto.

---
+++ mobile/src/routes.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import { createStackNavigator } from "@react-navigation/stack";
+import { NavigationContainer } from "@react-navigation/native";
+
+const AppStack = createStackNavigator();
+
+export default function Routes() {
+  return (
+    <NavigationContainer>
+      <AppStack.Navigator screenOptions={{ headerShown: false }}>
+      </AppStack.Navigator>
+    </NavigationContainer>
+  );
+}
