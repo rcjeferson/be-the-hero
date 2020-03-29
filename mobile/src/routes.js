146a34000b08b3b2e64374897e9979f01fafc9cc
Jeferson Costa <jeferson.costa@instruct.com.br>
2020-03-28 22:09:24 -0300

    feat(mobile): Adiciona tela de incidentes
    Adiciona tela para listar todos os incidentes para o usuário.

--- mobile/src/routes.js
+++ mobile/src/routes.js
@@ -3,12 +3,15 @@ import React from 'react';
 import { createStackNavigator } from "@react-navigation/stack";
 import { NavigationContainer } from "@react-navigation/native";
 
+import Incidents from './pages/Incidents';
+
 const AppStack = createStackNavigator();
 
 export default function Routes() {
   return (
     <NavigationContainer>
       <AppStack.Navigator screenOptions={{ headerShown: false }}>
+        <AppStack.Screen name="Incidents" component={Incidents} />
       </AppStack.Navigator>
     </NavigationContainer>
   );
