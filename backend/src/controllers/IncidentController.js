bf3ce762a6061eb72879137f1588df599e39ab5b
Jeferson Costa <jeferson.costa@instruct.com.br>
2020-03-28 21:55:44 -0300

    fix(Incident): Ajusta consulta lista
    Ajusta consulta que faz a listagem de incidentes, pois o join foi criado
    de forma errada e estava trazendo registros duplicados.

--- backend/src/controllers/IncidentController.js
+++ backend/src/controllers/IncidentController.js
@@ -5,7 +5,7 @@ module.exports = {
     const { page = 1} = request.query;
 
     const incidents = await connection('incidents')
-      .join('ongs', 'ong_id', '=', 'incidents.ong_id')
+      .join('ongs', 'ongs.id', '=', 'incidents.ong_id')
       .limit(5)
       .offset((page - 1) * 5)
       .select([
